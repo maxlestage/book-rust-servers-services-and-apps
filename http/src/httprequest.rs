360bc932842fa09daf35b9d6897619b6236e6101
maxlestage <maxlestage@icloud.com>
2022-08-29 17:22:11 +0200

    httprequest : ✅

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -58,6 +58,78 @@ impl From<&str> for Version {
     }
 }
 
+impl From<String> for HttpRequest {
+    fn from(req: String) -> Self {
+        let mut parsed_method = Method::Uninitialized;
+        let mut parsed_version = Version::V1_1;
+        let mut parsed_resource = Resource::Path("".to_string());
+        let mut parsed_headers = HashMap::new();
+        let mut parsed_msg_body = "";
+
+        // Read each line in the incoming HTTP request
+        for line in req.lines() {
+            // If the line read is request line, call function process_req_line()
+            if line.contains("HTTP") {
+                let (method, resource, version) = process_req_line(line);
+                parsed_method = method;
+                parsed_version = version;
+                parsed_resource = resource;
+            // If the line read is header line, call function process_header_line()
+            } else if line.contains(":") {
+                let (key, value) = process_header_line(line);
+                parsed_headers.insert(key, value);
+            //  If it is blank line, do nothing
+            } else if line.len() == 0 {
+                // If none of these, treat it as message body
+            } else {
+                parsed_msg_body = line;
+            }
+        }
+        // Parse the incoming HTTP request into HttpRequest struct
+        HttpRequest {
+            method: parsed_method,
+            version: parsed_version,
+            resource: parsed_resource,
+            headers: parsed_headers,
+            msg_body: parsed_msg_body.to_string(),
+        }
+    }
+}
+
+fn process_req_line(s: &str) -> (Method, Resource, Version) {
+    // Parse the request line into individual chunks split by whitespaces.
+    let mut words = s.split_whitespace();
+    // Extract the HTTP method from first part of the request line
+    let method = words.next().unwrap();
+    // Extract the resource (URI/URL) from second part of the request line
+    let resource = words.next().unwrap();
+    // Extract the HTTP version from third part of the request line
+    let version = words.next().unwrap();
+
+    (
+        method.into(),
+        Resource::Path(resource.to_string()),
+        version.into(),
+    )
+}
+
+fn process_header_line(s: &str) -> (String, String) {
+    // Parse the header line into words split by separator (':')
+    let mut header_items = s.split(":");
+    let mut key = String::from("");
+    let mut value = String::from("");
+    // Extract the key part of the header
+    if let Some(k) = header_items.next() {
+        key = k.to_string();
+    }
+    // Extract the value part of the header
+    if let Some(v) = header_items.next() {
+        value = v.to_string()
+    }
+
+    (key, value)
+}
+
 //  Test
 #[cfg(test)]
 mod test {
@@ -72,5 +144,19 @@ mod test {
     fn test_version_into() {
         let m: Version = "HTTP/1.1".into();
         assert_eq!(m, Version::V1_1);
+    }
+
+    #[test]
+    fn test_read_http() {
+        let s: String = String::from("GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
+        let mut headers_expected = HashMap::new();
+        headers_expected.insert("Host".into(), " localhost".into());
+        headers_expected.insert("Accept".into(), " */*".into());
+        headers_expected.insert("User-Agent".into(), " curl/7.64.1".into());
+        let req: HttpRequest = s.into();
+        assert_eq!(Method::Get, req.method);
+        assert_eq!(Version::V1_1, req.version);
+        assert_eq!(Resource::Path("/greeting".to_string()), req.resource);
+        assert_eq!(headers_expected, req.headers);
     }
 }
